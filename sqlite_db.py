9a43732d2835a2b272f232f46b9b857c8ec19e74
lbandeira <lbms@cin.ufpe.br>
2024-03-20 10:34:31 -0300

    fix(request): change port to request

--- sqlite_db.py
+++ sqlite_db.py
@@ -1,10 +1,17 @@
 import sqlite3
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+import json
 
 # 1. Ajustar para atualizar o database
 # 2. Utilizar o postman para envio do .json
 
 app = Flask(__name__)
+CORS(app)
+
+def read_devices_from_json():
+    with open('devices.json', 'r') as file:
+        return json.load(file)
 
 def update_database(data):
     connect = sqlite3.connect('device_info.db')
@@ -12,15 +19,20 @@ def update_database(data):
     # Criar tabela caso nao tenha
     cursor.execute(''' 
                 CREATE TABLE IF NOT EXISTS devices (
-                        id INTEGER PRIMARY KEY,
+                        id TEXT PRIMARY KEY,
                         name TEXT,
-                        location TEXT
+                        camera_count INTEGER
                     )
                 ''')
+    
+    # Apaga os dados existentes
+    cursor.execute("DELETE FROM devices")
+
     # Adiciona linha
-    for device in data['devices']:
-        cursor.execute("INSERT INTO devices (id, name, camera_count, location) VALUES (?, ?, ?, ?)",
-                  (device['id'], device['name'], len(device['cameras']), device['location']))
+    for devices in data:
+        query = f'''INSERT INTO devices (id, name, camera_count) VALUES ("{devices['id']}", "{devices['name']}", "{len(devices['cameras'])}")'''
+        print(query)
+        cursor.execute(query)
 
     connect.commit()
     connect.close()
@@ -28,7 +40,8 @@ def update_database(data):
 @app.route('/update_devices', methods=['POST'])
 
 def update_devices():
-    data = request.get_json()
+    #data = request.get_json()
+    data = read_devices_from_json()
     if data:
         update_database(data)
         return jsonify({"message": "Database updated successfuly"}), 200
@@ -36,4 +49,4 @@ def update_devices():
         return jsonify({"error": "Invalid JSON data"}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8080)
