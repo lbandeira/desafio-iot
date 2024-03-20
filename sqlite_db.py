9ef1408d7413c77dfed4129db68f12d82c1d0e24
lbandeira <lbms@cin.ufpe.br>
2024-03-19 23:44:43 -0300

    refactor(data_base): Refactor DB API using Flask

--- sqlite_db.py
+++ sqlite_db.py
@@ -1,24 +1,39 @@
 import sqlite3
+from flask import Flask, request, jsonify
 
-connect = sqlite3.connect('device_info.db')
-cursor = connect.cursor()
+# 1. Ajustar para atualizar o database
+# 2. Utilizar o postman para envio do .json
 
-cursor.execute(''' 
-               CREATE TABLE devices (
-                    id INTEGER PRIMARY KEY,
-                    name TEXT,
-                    location TEXT
-                  )
-               ''')
+app = Flask(__name__)
 
-cursor.execute('''
-                CREATE TABLE cameras (
-                    id INTEGER PRIMARY KEY,
-                    name TEXT,
-                    device_id INTEGER,
-                    FOREIGN KEY(device_id) REFERENCES devices(id)
-                  )
+def update_database(data):
+    connect = sqlite3.connect('device_info.db')
+    cursor = connect.cursor()
+    # Criar tabela caso nao tenha
+    cursor.execute(''' 
+                CREATE TABLE IF NOT EXISTS devices (
+                        id INTEGER PRIMARY KEY,
+                        name TEXT,
+                        location TEXT
+                    )
                 ''')
+    # Adiciona linha
+    for device in data['devices']:
+        cursor.execute("INSERT INTO devices (id, name, camera_count, location) VALUES (?, ?, ?, ?)",
+                  (device['id'], device['name'], len(device['cameras']), device['location']))
 
-connect.commit()
-connect.close()
+    connect.commit()
+    connect.close()
+
+@app.route('/update_devices', methods=['POST'])
+
+def update_devices():
+    data = request.get_json()
+    if data:
+        update_database(data)
+        return jsonify({"message": "Database updated successfuly"}), 200
+    else:
+        return jsonify({"error": "Invalid JSON data"}), 400
+
+if __name__ == "__main__":
+    app.run(debug=True)
