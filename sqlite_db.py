05a4ef6d2ab3ecbf02e56dfa30170abd4a039702
lbandeira <lbms@cin.ufpe.br>
2024-03-21 22:39:28 -0300

    feat(security): Add cryptography at sensitive data of the DB

--- sqlite_db.py
+++ sqlite_db.py
@@ -3,17 +3,25 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
 
-# 1. Ajustar para atualizar o database
-# 2. Utilizar o postman para envio do .json
+from cryptography.fernet import Fernet
 
 app = Flask(__name__)
 CORS(app)
+
+key = Fernet.generate_key()
+cipher = Fernet(key)
 
 def read_devices_from_json():
     with open('devices.json', 'r') as file:
         return json.load(file)
 
 def update_database(data):
+    # Aplicar camada de seguranca em dados sensiveis
+    for device in data:
+        if 'password'in device:
+            encrypted_password = cipher.encrypt(device['password'].encode())
+            device['password'] = encrypted_password.decode()
+
     connect = sqlite3.connect('device_info.db')
     cursor = connect.cursor()
     # Criar tabela caso nao tenha
@@ -21,27 +29,43 @@ def update_database(data):
                 CREATE TABLE IF NOT EXISTS devices (
                         id TEXT PRIMARY KEY,
                         name TEXT,
-                        camera_count INTEGER
+                        camera_count INTEGER,
+                        password TEXT
                     )
                 ''')
     
     # Apaga os dados existentes
     cursor.execute("DELETE FROM devices")
 
-    # Adiciona linha
+    # Adiciona linha na tabela
     for devices in data:
-        query = f'''INSERT INTO devices (id, name, camera_count) VALUES ("{devices['id']}", "{devices['name']}", "{len(devices['cameras'])}")'''
+        query = f'''INSERT INTO devices (id, name, camera_count, password) VALUES ("{devices['id']}", "{devices['name']}", "{len(devices['cameras'])}", "{devices['password']}")'''
         print(query)
         cursor.execute(query)
 
     connect.commit()
     connect.close()
 
+def decrypted():
+    # Retrieve encrypted data from SQLite database and decrypt
+    conn = sqlite3.connect('secure_devices.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM devices;')
+    for row in cursor.fetchall():
+        decrypted_password = cipher.decrypt(row[3].encode()).decode()
+        print(f"Name: {row[1]}, Username: {row[2]}, Decrypted Password: {decrypted_password}")
+
+    conn.close()    
+
 @app.route('/update_devices', methods=['POST'])
 
 def update_devices():
-    #data = request.get_json()
-    data = read_devices_from_json()
+    #Recebe JSON pela requisicao
+    data = request.get_json()
+
+    #Faz leitura do JSON de forma local
+    #data = read_devices_from_json()
     if data:
         update_database(data)
         return jsonify({"message": "Database updated successfuly"}), 200
